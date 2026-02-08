09ca528b1add6f42697c083a7d0764755a24877c
VKQM5303 <vukienquoc53@gmail.com>
2026-02-08 20:17:49 +0700

    Initial commit

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Chắc chưa dị?",
+    "THIỆT là chấc hong??",
+    "A DU SURE?",
+    "Cô pé à...",
+    "Tôi nói em suy nghĩ lại!",
+    "Em nói hong tui buồn lắm ó...",
+    "Buồn lắm á nha...",
+    "Nghĩ quẩn lun đó...",
+    "Oke fine, tui hong thèm...",
+    "Xạo chó á, nói có dùm cái điiiii! ❤️"
 ];
 
 let messageIndex = 0;
