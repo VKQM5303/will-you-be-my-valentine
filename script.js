8e64c7895cd2820bdce35d07dc41bdf26cf50b5f
VKQM5303 <vukienquoc53@gmail.com>
2026-02-14 12:18:04 +0700

    Play music on user interaction

--- script.js
+++ script.js
@@ -13,15 +13,34 @@ const messages = [
 
 let messageIndex = 0;
 
+// prepare audio (NOT autoplay)
+const loveAudio = new Audio("love.mp3");
+loveAudio.loop = true;
+let audioUnlocked = false;
+
+function startMusic() {
+    if (!audioUnlocked) {
+        loveAudio.play().catch(() => {});
+        audioUnlocked = true;
+    }
+}
+
 function handleNoClick() {
-    const noButton = document.querySelector('.no-button');
-    const yesButton = document.querySelector('.yes-button');
+    startMusic();
+
+    const noButton = document.querySelector(".no-button");
+    const yesButton = document.querySelector(".yes-button");
+
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
-    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+
+    const currentSize = parseFloat(
+        window.getComputedStyle(yesButton).fontSize
+    );
     yesButton.style.fontSize = `${currentSize * 1.5}px`;
 }
 
 function handleYesClick() {
+    startMusic();
     window.location.href = "yes_page.html";
 }
